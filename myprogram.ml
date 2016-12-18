ba790718c2c33ac2f7f74ae7b9ef79288c615547
Marek Kubica <marek@xivilization.net>
2016-12-18 17:30:20 +0100

    Add some comments to what the code is doing

--- myprogram.ml
+++ myprogram.ml
@@ -1,12 +1,74 @@
 type cube = Cube of int
 
+exception Too_big
+
+(* When we extract a cube out of a cuboid, it splits into 0-3 parts *)
+type 'a subdivisions =
+  (* When the cube matches 3 dimensions of the cuboid, it matches it completely, no rest *)
+  | Zero
+  (* When the cube matches 2 dimensions of the cuboid *)
+  | One of ('a * 'a * 'a)
+  (* When the cube matches 1 dimension *)
+  | Two of ('a * 'a * 'a) * ('a * 'a * 'a)
+  (* When the cube is smaller than all of the cuboid dimensions *)
+  | Three of ('a * 'a * 'a) * ('a * 'a * 'a) * ('a * 'a * 'a)
+
+let extract1 (x, y, z) (Cube n) =
+  if n > x || n > y || n > z then
+    (* Trying to pull out a Cube of size n of something that is of less than n? *)
+    raise Too_big
+  else
+    (* Calculate the count and sizes of the respective split cuboids *)
+    match (n = x, n = y, n = z) with
+    | true, true, true -> Zero
+    | true, true, false -> One (x, y, z-n)
+    | true, false, true -> One (x, y-n, z)
+    | false, true, true -> One (x-n, y, z)
+    | false, false, true -> Two ((x-n, y, z), (n, y-n, z))
+    | false, true, false -> Two ((n, y, z-n), (x-n, y, z))
+    | true, false, false -> Two ((x, n, z-n), (x, y-n, z))
+    | false, false, false -> Three ((n, n, z-n), (x-n, n, z), (x, y-n, z))
+
+exception Impossibru
+
+let rec extract geom = function
+  (* We ran out of cubes to extract from the geometry *)
+  | [] -> raise Impossibru
+  (* Try to extract the first cube *)
+  | cube::cubes -> match extract1 geom cube with
+    | exception Too_big ->
+        (* That cube was too big, try again with the next *)
+        let (matches, cubes) = extract geom cubes in
+        (matches, cube::cubes)
+    | Zero -> (1, cubes)
+    (* Then we just recurse on the different ways the cuboids could be split *)
+    | One geom ->
+        let (matches, cubes) = extract geom cubes in
+        (matches + 1, cubes)
+    | Two (geom1, geom2) ->
+        let (matches1, cubes) = extract geom1 cubes in
+        let (matches2, cubes) = extract geom2 cubes in
+        (matches1 + matches2 + 1, cubes)
+    | Three (geom1, geom2, geom3) ->
+        let (matches1, cubes) = extract geom1 cubes in
+        let (matches2, cubes) = extract geom2 cubes in
+        let (matches3, cubes) = extract geom3 cubes in
+        (matches1 + matches2 + matches3 + 1, cubes)
+
+let solve ((x, y, z), cubes) =
+  match extract (x, y, z) cubes with
+  | (i, _) -> i
+  | exception Impossibru -> -1
+
 let parse line =
+  (* no \d? okay.jpg *)
   let r = Str.regexp "\\([0-9]+\\) \\([0-9]+\\) \\([0-9]+\\)\\(.*\\)" in
   (* wtf what a horrible API *)
   let _ = Str.string_match r line 0 in
   let x = int_of_string @@ Str.matched_group 1 line in
   let y = int_of_string @@ Str.matched_group 2 line in
   let z = int_of_string @@ Str.matched_group 3 line in
+  (* Convert all these numbers into n times Cube (position ** 2) *)
   let cubes =
     Str.matched_group 4 line
     |> Str.split (Str.regexp " ")
@@ -21,56 +83,6 @@ let parse line =
     |> List.rev
   in
   ((x, y, z), cubes)
-
-exception Too_big
-
-type 'a subdivisions =
-  | Zero
-  | One of ('a * 'a * 'a)
-  | Two of ('a * 'a * 'a) * ('a * 'a * 'a)
-  | Three of ('a * 'a * 'a) * ('a * 'a * 'a) * ('a * 'a * 'a)
-
-let extract (x, y, z) (Cube n) =
-  if n > x || n > y || n > z then
-    raise Too_big
-  else
-    match (n = x, n = y, n = z) with
-    | true, true, true -> Zero
-    | true, true, false -> One (x, y, z-n)
-    | true, false, true -> One (x, y-n, z)
-    | false, true, true -> One (x-n, y, z)
-    | false, false, true -> Two ((x-n, y, z), (n, y-n, z))
-    | false, true, false -> Two ((n, y, z-n), (x-n, y, z))
-    | true, false, false -> Two ((x, n, z-n), (x, y-n, z))
-    | false, false, false -> Three ((n, n, z-n), (x-n, n, z), (x, y-n, z))
-
-exception Impossibru
-
-let rec helper geom cubes =
-  match cubes with
-  | [] -> raise Impossibru
-  | cube::rest -> match extract geom cube with
-    | exception Too_big ->
-        let (matches, rest') = helper geom rest in
-        (matches, cube::rest')
-    | Zero -> (1, rest)
-    | One geom ->
-        let (matches, rest') = helper geom rest in
-        (matches + 1, rest')
-    | Two (geom1, geom2) ->
-        let (matches1, rest') = helper geom1 rest in
-        let (matches2, rest'') = helper geom2 rest' in
-        (matches1 + matches2 + 1, rest'')
-    | Three (geom1, geom2, geom3) ->
-        let (matches1, rest') = helper geom1 rest in
-        let (matches2, rest'') = helper geom2 rest' in
-        let (matches3, rest''') = helper geom3 rest'' in
-        (matches1 + matches2 + matches3 + 1, rest''')
-
-let solve ((x, y, z), cubes) =
-  match helper (x, y, z) cubes with
-  | (i, _) -> i
-  | exception Impossibru -> -1
 
 let process line =
   line
